f344a6de4477d8f8965a157cd35bc8593148c534
George Maddocks <georgemaddocks@yahoo.co.uk>
2015-11-11 18:14:11 +0000

    HTML and Jquery experimentation

---
+++ ready.js
@@ -0,0 +1,19 @@
+$( document ).ready(function() {
+// whatever we put below will be loaded immediately by the page.
+
+    // $("#temperature-up").hide( "slow" );
+
+     $("#powersaving-on").on("click", function() {
+
+$("#powersaving-off").toggle();
+
+});
+
+$("#powersaving-on").on("click", function() {
+
+$("#powersaving-off").toggle();
+
+});
+
+
+});
