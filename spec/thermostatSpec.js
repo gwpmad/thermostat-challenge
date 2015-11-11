8070bb276575404047e2b5ab4bd1faa3928ec825
George Maddocks <georgemaddocks@yahoo.co.uk>
2015-11-11 15:49:21 +0000

    most ch 5 user stories accounted for

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -9,13 +9,41 @@ describe("Thermostat", function(){
     expect(thermostat.temperature).toEqual(20);
   });
 
+  it('is on power saving mode as default', function(){
+    expect(thermostat.powerSaving).toEqual(true);
+  });
+
   it('increase the temperature by 1', function(){
-    thermostat.up();
+    thermostat.up(1);
     expect(thermostat.temperature).toEqual(21);
   });
 
   it('decrease the temperature by 1', function(){
-    thermostat.down();
+    thermostat.down(1);
     expect(thermostat.temperature).toEqual(19);
   });
+
+  it('raises an error when trying to take temp below 10', function(){
+    thermostat.down(10);
+    expect(function(){thermostat.down(1);}).toThrowError("10 is the minimum");
+  });
+
+  it('raises an error when trying to take temp above 25 (power saving on)', function(){
+    thermostat.up(5);
+    expect(function(){thermostat.up(1);}).toThrowError("25 is the max when saving power");
+  });
+
+  it('raises an error when trying to take temp above 32 (power saving off)', function(){
+    thermostat.powerSavingSwitch();
+    thermostat.up(12);
+    expect(function(){thermostat.up(1);}).toThrowError("32 is the max when not saving power");
+  });
+
+  it('resets the temperature to 20', function(){
+    thermostat.up(1);
+    thermostat.temperatureReset();
+    expect(thermostat.temperature).toEqual(20);
+  });
+
+
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,12 +1,30 @@
 function Thermostat(){
-  this.temperature = 20;
+  this.DEFAULT_TEMPERATURE = 20;
+  this.temperature = this.DEFAULT_TEMPERATURE;
+  this.powerSaving = true;
 }
 
 Thermostat.prototype = {
-  up: function(){
-    this.temperature += 1;
+  powerSavingSwitch: function(){
+    this.powerSaving = !(this.powerSaving);
   },
-  down: function(){
-    this.temperature -= 1;
+
+  up: function(increment){
+    if(this.powerSaving === false && (this.temperature + increment) > 32) {
+        throw new Error("32 is the max when not saving power");}
+    else if(this.powerSaving === true && (this.temperature + increment) > 25) {
+        throw new Error("25 is the max when saving power");}
+    else{
+        this.temperature += increment;
+      }},
+
+  down: function(decrement){
+    if(this.temperature === 10) {
+      throw new Error("10 is the minimum");}
+    else { this.temperature -= decrement;}
+      },
+
+  temperatureReset: function(){
+    this.temperature = 20;
   }
 };
